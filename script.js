fdf8aabc8bad57231aff028b451c34cb9a120cad
Abhishek Pratap Singh <spratapsingh662@gmail.com>
2025-10-25 20:10:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".join-btn").forEach(button => {
+  button.addEventListener("click", () => {
+    alert("You have successfully registered for the tournament! 🎮");
+  });
+});
+
+document.getElementById("joinCommunity").addEventListener("click", () => {
+  alert("Welcome to the Game Sikho Community! 🚀");
+});
